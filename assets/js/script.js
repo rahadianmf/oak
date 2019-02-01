439258e846c6df3c59f3affec52c35e80e20c24b
Rahadian Muhammad <rahadianmf@hotmail.com>
2019-02-01 09:28:40 +0700

    html done 1366-1920

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,18 @@ $('.about-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
 	autoplay: true,
-	dots: true,
+	dots: false,
 	prevArrow: false,
 	nextArrow: false,
   autoplaySpeed: 2000,
+});
+
+$('.slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+	autoplay: true,
+	dots: false,
+	prevArrow: false,
+	nextArrow: false,
+  autoplaySpeed: 1500,
 });
